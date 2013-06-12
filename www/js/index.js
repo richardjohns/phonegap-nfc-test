4271e64de2506a86aacb11b24c89328783e79ea6
Don Coleman <dcoleman@chariotsolutions.com>
2013-06-12 00:12:45 -0400

    call nfc functions

--- www/js/index.js
+++ www/js/index.js
@@ -9,6 +9,30 @@ var app = {
         // note that this is an event handler so the scope is that of the event
         // so we need to call app.report(), and not this.report()
         app.report('deviceready');
+        
+        // Read NDEF formatted NFC Tags
+        nfc.addNdefListener (
+            function (nfcEvent) {
+                var tag = nfcEvent.tag,
+                    ndefMessage = tag.ndefMessage;
+
+                // dump the raw json of the message
+                // note: real code will need to decode
+                // the payload from each record
+                alert(JSON.stringify(ndefMessage));
+
+                // assuming the first record in the message has 
+                // a payload that can be converted to a string.
+                alert(nfc.bytesToString(ndefMessage[0].payload).substring(3));
+            }, 
+            function () { // success callback
+                // Don't use an alert in real code, this will just confirm things are working
+                alert("Waiting for NDEF tag");
+            },
+            function (error) { // error callback
+                alert("Error adding NDEF listener " + JSON.stringify(error));
+            }
+        );
     },
     report: function(id) { 
         console.log("report:" + id);
