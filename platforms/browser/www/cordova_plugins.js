fab2e92696a619898830f118a3e5c110a32ac26f
richardjohns <you@example.com>
2017-10-15 17:53:02 +1300

    first commit

---
+++ platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,16 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.chariotsolutions.nfc.plugin/www/phonegap-nfc.js",
+        "id": "com.chariotsolutions.nfc.plugin.NFC",
+        "pluginId": "com.chariotsolutions.nfc.plugin",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.chariotsolutions.nfc.plugin": "0.4.4-SNAPSHOT"
+}
+// BOTTOM OF METADATA
+});
